d8189ac4c0f2960dd92ba1f662f7de027c747151
Povilas <167984240+Povilas88@users.noreply.github.com>
2024-08-28 09:23:38 +0300

    /account -delete

--- router/accountRouter.js
+++ router/accountRouter.js
@@ -55,6 +55,31 @@ accountRouter.get('/:name-:surname', (req, res) => {
     }
 });
 
+accountRouter.delete('/:name-:surname', (req, res) => {
+    const { name, surname } = req.params;
+
+    if (userData.length === 0) {
+        return res.status(404).json({ error: 'User data array is empty.' });
+    }
+
+    const userIndex = userData.findIndex(user =>
+        user.name.toLowerCase() === name.toLowerCase() &&
+        user.surname.toLowerCase() === surname.toLowerCase()
+    );
+    const user = userData[userIndex];
+
+    if (userIndex !== -1) {
+        if (user.balance === 0) {
+            userData.splice(userIndex, 1);
+            return res.status(200).json({ success: `User "${user.name} ${user.surname}" has been successfully deleted.` });
+        } else {
+            return res.status(400).json({ error: `Deletion failed: User "${user.name} ${user.surname}" has a balance of ${user.balance} USD.` });
+        }
+    } else {
+        return res.status(404).json({ error: `User: "${name} ${surname}" not found.` });
+    }
+});
+
 accountRouter.put('/:name-:surname', (req, res) => {
     const { name, surname } = req.params;
     const { newName, newSurname, newBirthDate } = req.body;
